1b260beeaa519280af2a6dedb84d68771188f3e7
Reza Karamad <r.karamad@gmail.com>
2026-06-16 22:37:11 +0200

    update deps

---
+++ modules/nextinsight/doc.go
@@ -0,0 +1,6 @@
+// Package nextinsight provides a lightweight client for the Next-Insight REST API v3.
+//
+// It fetches application metadata and group associations and assembles them
+// into an AppMetadata value that can be stamped onto Kubernetes resource
+// labels and annotations.
+package nextinsight
